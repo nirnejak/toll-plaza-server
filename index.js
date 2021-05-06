a925cd23f9122e85690675a1afa5a256c2c2aeda
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2021-05-06 15:07:07 +0530

    VSCode Config Removed

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("dotenv").config()
 const chalk = require("chalk")
 const cors = require("cors")
 const express = require("express")
+const mongoose = require("mongoose")
 
 const { logger } = require("./middlewares/logger")
 
@@ -13,11 +14,20 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(logger)
 
-// app.use("/api/users", require("./routes/api/users"))
+app.use("/api/tolls", require("./routes/api/tolls"))
 
 app.get("*", (req, res) => {
   res.status(404).json({ error: true, message: "Not Found" })
 })
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log(chalk.green("🔥  MongoDB Connected...")))
+  .catch((err) => console.log(chalk.red(err)))
 
 app.listen(process.env.PORT, () => {
   console.log(chalk.green(`👍  Server started at PORT: ${process.env.PORT}`))
--- routes/api/tolls.js
+++ routes/api/tolls.js
@@ -1,28 +1,37 @@
-// const chalk = require("chalk")
+const chalk = require("chalk")
 const express = require("express")
 
-const isAuthenticated = require("../../middlewares/auth")
-// const Toll = require("../../models/toll")
+const Toll = require("../../models/toll")
 
 const router = express.Router()
 
-router.get("/", isAuthenticated, (req, res) => {
-  res.status(501).send("List Tolls")
+router.get("/", (req, res) => {
+  Toll.find()
+    .then((tolls) => res.json(tolls))
+    .catch((err) => {
+      console.log(chalk.red(err))
+      res.status(500).json({ message: "Something went wrong" })
+    })
 })
 
-router.get("/:id", isAuthenticated, (req, res) => {
-  res.status(501).send("Get a Toll")
+router.get("/:id", (req, res) => {
+  Toll.findOne({ _id: req.params.id })
+    .then((toll) => res.json(toll))
+    .catch((err) => {
+      console.log(chalk.red(err))
+      res.status(500).json({ message: "Something went wrong" })
+    })
 })
 
-router.post("/", isAuthenticated, (req, res) => {
+router.post("/", (req, res) => {
   res.status(501).send("Create a User")
 })
 
-router.put("/:id", isAuthenticated, (req, res) => {
+router.put("/:id", (req, res) => {
   res.status(501).send("Update a User")
 })
 
-router.delete("/:id", isAuthenticated, (req, res) => {
+router.delete("/:id", (req, res) => {
   res.status(501).send("Delete a User")
 })
 
